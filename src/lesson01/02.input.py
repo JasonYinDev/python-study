d04b9b63a4a41f7a93a40c713c8ecd3b33ba4504
ayin86cn <ayin86cn@gmail.com>
2023-09-03 17:55:42 +0800

    lesson01

---
+++ src/lesson01/02.input.py
@@ -0,0 +1,9 @@
+# a=input("input your age")
+# print(a)
+
+price=input("please input price")
+weight=input("please input weight")
+finalPrice=float(price)*float(weight)
+print(f"the price is {finalPrice}")
+# print("the price is %f"%(price*weight))
+
