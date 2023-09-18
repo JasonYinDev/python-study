f77a2f86e04f0305c058e338a9bcaf454e33a96e
ayin86cn <ayin86cn@gmail.com>
2023-09-18 18:13:45 +0800

    blog?

---
+++ src/blog/main.py
@@ -0,0 +1,56 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.openapi.docs import get_redoc_html, get_swagger_ui_html
+from fastapi.staticfiles import StaticFiles
+
+# app = FastAPI()
+
+app = FastAPI(docs_url=None, redoc_url=None)
+
+app.mount('/static', StaticFiles(directory='static'))
+
+# <editor-fold desc="docs">
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - Swagger UI",
+            oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+            swagger_js_url="/static/swagger/swagger-ui-bundle.js",
+            swagger_css_url="/static/swagger/swagger-ui.css")
+
+
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+            openapi_url=app.openapi_url,
+            title=app.title + " - ReDoc",
+            redoc_js_url="/static/redoc/redoc.standalone.js")
+# </editor-fold>
+
+
+#添加首页
+@app.get("/")
+def index():
+  return "this is home page"
+
+
+@app.get("/users")
+def users():
+  return {
+    "msg":"Get all users",
+    "code":200
+  }
+  
+@app.get("/projects")
+def projects():
+  return [
+    "tehcui-vue2",
+    "tehcui-vue3-lite",
+    "tehcui-react-lite",
+    "tehcui-toolkit",
+  ] 
+
+if __name__=="__main__":
+  uvicorn.run("main:app",reload=True)
+  # uvicorn.run(app) 
---
+++ src/blog/mysql.py
@@ -0,0 +1,32 @@
+import pymysql
+
+# 链接数据库
+conn =pymysql.connect(host="127.0.0.1",port=3306,user="blogadmin",passwd="passw0rd",charset="utf8")
+cursor=conn.cursor()
+
+# 创建数据库
+# cursor.execute("create database db4 default charset utf8 collate utf8_general_ci")
+# conn.commit()
+
+# 进入数据库
+# cursor.execute("use blog")
+# cursor.execute("show tables")
+# result=cursor.fetchall()
+# print(result)
+
+# sql="""
+# create table L3(
+#   id int not null primary key auto_increment,
+#   title varchar(123),
+#   content text,
+#   ctime datetime
+# )default charset=utf8;
+# """
+
+# cursor.execute("show tables")
+# result=cursor.fetchall()
+# print(result)
+
+# # 关闭链接
+# cursor.close()
+# conn.close()
